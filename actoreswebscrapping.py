39c13d712c448348737a370080671d1dd96d0465
Ronald Castillo Capino <6721335+R0N4L2@users.noreply.github.com>
2019-05-13 18:29:51 -0500

    Webscrapping Actors
    webscrapping actors from IMDb for face recognition

---
+++ actoreswebscrapping.py
@@ -0,0 +1,39 @@
+import urllib3
+import json
+import datetime
+from bs4 import BeautifulSoup
+import os
+import gc
+import pandas as pd
+import numpy as np
+from os.path import dirname, abspath, join, exists
+"""SE CONECTA A IMDB PARA OBTENER FOTOS DE VARIOS ARTISTAS POR NOMBRE Y GENERO"""
+GENERO=["female","male"]
+http = urllib3.PoolManager()
+di = join(dirname(abspath('')),'imagenes\\')
+if not exists(di):
+    os.mkdir(di)
+for gen in GENERO:
+    urlFoLaction = "https://www.imdb.com/search/name?gender="+gen+"&start=1&ref_=rlm"
+    response = http.request('GET',urlFoLaction)
+    soup = BeautifulSoup(response.data)
+    s=pd.Series(soup.find_all("span"))
+    cant=int(str(s[19]).split(' ')[2].replace(",",""))
+    if not exists(join(di,gen+'\\')):
+        os.mkdir(join(di,gen+'\\'))
+    di1=join(di,gen+'\\')
+    for k in range(int(np.ceil(cant/50))):
+        urlFoLaction = "https://www.imdb.com/search/name?gender="+gen+"&start="+str(1+k)+"&ref_=rlm"
+        response = http.request('GET',urlFoLaction)
+        soup = BeautifulSoup(response.data)
+        s=pd.Series(soup.find_all("img"))
+        A=s[4:-1].astype(str).str.split("=",expand=True)
+        A=A[[1,3]]
+        A[1]=A[1].str.split('"',expand=True)[1]
+        A[3]=A[3].str.split('"',expand=True)[1]
+        A=A.reset_index(drop=True)
+        for k in range(A.shape[0]):
+            f = open(di1+A[1].iloc[k].replace(" ","_")+'.jpg','wb')
+            r = http.request('GET',A[3].iloc[k])
+            f.write(r.content)
+            f.close()
