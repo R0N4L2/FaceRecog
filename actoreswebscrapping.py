1673db45e5f0df8972ab144cb3ff03ea1b0f420a
Ronald Castillo Capino <6721335+R0N4L2@users.noreply.github.com>
2019-05-15 13:24:48 -0500

    Update webscrapping algorithm
    In this update, it can web scrapping pictures of last year to each actor.

--- actoreswebscrapping.py
+++ actoreswebscrapping.py
@@ -22,6 +22,7 @@ http = urllib3.PoolManager()
 di = join(dirname(abspath('')),'imagenes\\')
 wiki="https://en.wikipedia.org/wiki/"
 start=True
+siz=1
 if not exists(di):
     os.mkdir(di)
 for gen in GENERO:
@@ -39,41 +40,78 @@ for gen in GENERO:
         soup = BeautifulSoup(response.data)
         s=pd.Series(soup.find_all("img"))
         A=s[4:-1].astype(str).str.split("=",expand=True)
-        A=A[[1,3]]
+        A=A.drop(columns=list(set(A.columns.tolist())-{1,3}))
         A[1]=A[1].str.split('"',expand=True)[1]
         A[3]=A[3].str.split('"',expand=True)[1]
         A=A.reset_index(drop=True)
         for k in range(A.shape[0]):
             filename=di1+A[1].iloc[k].replace(" ","_")
             url=A[3].iloc[k]
-            n=int(url.find("nopicture")>0)
+            if url.find("nopicture")<0:
+                n=int(url.find("nopicture")<0)
+                with http.request('GET',url, preload_content=False) as resp, open(filename+'.jpg', 'wb') as out_file:
+                    shutil.copyfileobj(resp, out_file)
+                resp.release_conn()
             response =http.request('GET',wiki+A[1].iloc[k].replace(" ","_"))
             soup = BeautifulSoup(response.data)
             dt=pd.Series(soup.find_all("tbody"))
             s2=BeautifulSoup(str(dt[0]))
+            im2=[]
             if len(s2.find_all("img"))>0:
                 n+=1
-                img2="https:"+str(s2.find_all("img")[0]).split(" ")[-3]
-                with http.request('GET',img2, preload_content=False) as resp, open(filename+'_'+str(n)+'.jpg', 'wb') as out_file:
-                    shutil.copyfileobj(resp, out_file)
-                resp.release_conn()
+                aa1=pd.Series(str(s2.find_all("img")[0]).split('"'))
+                ss=aa1.str.find(".jpg")
+                aa1=aa1[ss>0].reset_index(drop=True)
+                ss=aa1.str.find("wikipedia")
+                aa1=aa1[ss>0].reset_index(drop=True)
+                if any(aa1.str.find(",")>0):
+                    aa1=aa1.str.split(',',expand=True)
+                    aa1=aa1.values.reshape(aa1.size)
+                    aa1=pd.Series(list(set(aa1.tolist())-{None})).str.split(' ',expand=True)
+                    aa1=pd.Series(list(set(aa1.values.reshape(aa1.size).tolist())-{None}))
+                    ss=aa1.str.find(".jpg/")
+                    aa1='https:'+pd.unique(aa1[ss>0].str.split('.jpg/',expand=True)[0])+'.jpg'
+                    for g,h in enumerate(aa1):                
+                        im2+=[g+n]
+                        with http.request('GET',h, preload_content=False) as resp, open(filename+'_'+str(g+n)+'.jpg', 'wb') as out_file:
+                            shutil.copyfileobj(resp, out_file)
+                        resp.release_conn()
+            #Photography of last year
+            response =http.request('GET',"https://www.gettyimages.es/fotos/{}?compositions=headshot&family=editorial&numberofpeople=one&recency=last12months&sort=best".format(A[1].iloc[k].lower().replace(" ","_")))
+            soup = BeautifulSoup(response.data)
+            s3=soup.find_all("img")
+            ultima=False
+            if s3>0:
+                S=pd.Series(s3).astype(str)
+                P=S.str.find(A[1].iloc[k])
+                if any(P>0):
+                    n+=1
+                    ultima=True
+                    S=S[P>0].reset_index(drop=True)
+                    S=S.str.split('"',expand=True)[5]
+                    aa=np.random.randint(S.shape[0], size=siz).tolist()
+                    for g,h in enumerate(S.iloc[aa]):
+                        with http.request('GET',h, preload_content=False) as resp, open(filename+'_lastyear_'+str(g)+'.jpg', 'wb') as out_file:
+                            shutil.copyfileobj(resp, out_file)
+                        resp.release_conn()
             if n>0:
                 name=A[1].iloc[k]
                 bday=str(s2.find_all("span")[1]).split('"bday">')[1].split("</span>")[0]
                 old=humanfriendly.format_timespan(datetime.datetime.today()-datetime.datetime.strptime(bday, '%Y-%m-%d'),max_units=1)
                 old=int(old.split(" ")[0])
                 dic={'Nombre':[name],"genero":[gen],"Nacimiento":[bday],"Edad":[old]}
-                if url.find("nopicture")>0:
+                if url.find("nopicture")<0:
                     dic.update({"img1":[filename+'.jpg']})
-                if len(s2.find_all("img"))>0:
-                    dic.update({"img2":[filename+'_'+str(n)+'.jpg']})
+                if len(img2)>0:
+                    for g in img2:
+                        dic.update({"img"+str(k):[filename+'_'+str(g)+'.jpg']})
+                if ultima:
+                    for g in range(len(aa)):
+                        dic.update({"img_lastyear"+str(g):[filename+'_lastyear_'+str(g)+'.jpg']})
                 S=pd.DataFrame.from_dict(dic)
                 if start:
                     DFR=S
                     start=False
                 else:
-                    DFR=DFR.append(S)
-                with http.request('GET',url, preload_content=False) as resp, open(filename+'.jpg', 'wb') as out_file:
-                    shutil.copyfileobj(resp, out_file)
-                resp.release_conn()
+                    DFR=DFR.append(S)                
 writePickle(DFR,di+'FaceRecognition.pickle')
