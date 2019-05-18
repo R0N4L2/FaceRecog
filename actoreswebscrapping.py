b152d4ef1c12d7d02e9df6d8f2e11e75ae48496e
Ronald Castillo Capino <6721335+R0N4L2@users.noreply.github.com>
2019-05-18 16:53:16 -0500

    Add files via upload

--- actoreswebscrapping.py
+++ actoreswebscrapping.py
@@ -11,56 +11,58 @@ import shutil
 from os.path import dirname, abspath, join, exists
 import pickle
 
-def writePickle(df,fileName):
-	with open(fileName, 'wb') as f:
+def writePickle(df,file_name):
+	with open(file_name, 'wb') as f:
 		pickle.dump(df, f, pickle.HIGHEST_PROTOCOL)
-def web4photo(web,fileName):
+def web4photo(web,file_name):
     http = urllib3.PoolManager()
-    with http.request('GET',web, preload_content=False) as resp, open(fileName, 'wb') as out_file:
+    with http.request('GET',web, preload_content=False) as resp, open(file_name, 'wb') as out_file:
         shutil.copyfileobj(resp, out_file)
     resp.release_conn()
-def writeCSV(df,fileName,useIndex = False):
-	df.to_csv(fileName,index=useIndex)	
-def writeExcel(df,fileName,useIndex = False):
-	df.to_excel(fileName,index=useIndex)
+def writeCSV(df,file_name,useIndex = False):
+	df.to_csv(file_name,index=useIndex)	
+def writeExcel(df,file_name,useIndex = False):
+	df.to_excel(file_name,index=useIndex)
 def writeFile(df,filName,useIndex = False):
-	if fileName.endswith('.csv'):
-		writeCSV(df,fileName,useIndex = useIndex)
-	if fileName.endswith('.xlsx'):
-		writeExcel(df,filename,useIndex = useIndex)
-	if fileName.endsWith('.pickle'):
-		writePickle(df,fileName)   
-def readFile(fileName):
-	if fileName.endswith('.csv'):
-		return pandas.read_csv(fileName)
-	if fileName.endswith('.xlsx'):
-		return pandas.read_excel(fileName)
-	if fileName.endsWith('.pickle'):
-		with open(fileName, 'rb') as f:
+	if file_name.endswith('.csv'):
+		writeCSV(df,file_name,useIndex = useIndex)
+	if file_name.endswith('.xlsx'):
+		writeExcel(df,file_name,useIndex = useIndex)
+	if file_name.endsWith('.pickle'):
+		writePickle(df,file_name)   
+def readFile(file_name):
+	if file_name.endswith('.csv'):
+		return pandas.read_csv(file_name)
+	if file_name.endswith('.xlsx'):
+		return pandas.read_excel(file_name)
+	if file_name.endsWith('.pickle'):
+		with open(file_name, 'rb') as f:
 			return pickle.load(f)
-    
+
+def changeFormat(x):
+    return [x.lower().replace(" ","_"),x.replace(" ","%20")]   
 """SE CONECTA A IMDB PARA OBTENER FOTOS DE VARIOS ARTISTAS POR NOMBRE Y GENERO"""
 GENERO=["female","male"]
 http = urllib3.PoolManager()
 di = join(dirname(abspath('')),'imagenes\\')
-wiki="https://en.wikipedia.org/wiki/"
+url_wiki="https://en.wikipedia.org/wiki/"
 start=True
 print('Ingrese la cantidad de fotos que quiere rescatar:')
-photosTotal=int(input())
+photos_totales=int(input())
 if not exists(di):
     os.mkdir(di)
 for gen in GENERO:
-    urlFoLaction = "https://www.imdb.com/search/name?gender="+gen+"&start=1&ref_=rlm"
-    response = http.request('GET',urlFoLaction)
+    url_imdb = "https://www.imdb.com/search/name?gender="+gen+"&start=1&ref_=rlm"
+    response = http.request('GET',url_imdb)
     soup = BeautifulSoup(response.data)
     s=pd.Series(soup.find_all("span"))
-    cant=int(str(s[19]).split(' ')[2].replace(",",""))
+    actores_totales=int(str(s[19]).split(' ')[2].replace(",",""))
     if not exists(join(di,gen+'\\')):
         os.mkdir(join(di,gen+'\\'))
     di1=join(di,gen+'\\')
-    for k in range(0,min(cant,10000),50):
-        urlFoLaction = "https://www.imdb.com/search/name?gender="+gen+"&start="+str(1+k)+"&ref_=rlm"
-        response = http.request('GET',urlFoLaction)
+    for k in range(0,min(actores_totales,10000),50):
+        url_imdb = "https://www.imdb.com/search/name?gender="+gen+"&start="+str(1+k)+"&ref_=rlm"
+        response = http.request('GET',url_imdb)
         soup = BeautifulSoup(response.data)
         s=pd.Series(soup.find_all("img"))
         A=s[4:-1].astype(str).str.split("=",expand=True)
@@ -69,12 +71,12 @@ for gen in GENERO:
         A[3]=A[3].str.split('"',expand=True)[1]
         A=A.reset_index(drop=True)
         for k in range(A.shape[0]):
-            filename=di1+A[1].iloc[k].replace(" ","_")
-            url=A[3].iloc[k]
-            if url.find("nopicture")<0:
-                n=int(url.find("nopicture")<0)
-                web4photo(url,filename+'.jpg')
-            response =http.request('GET',wiki+A[1].iloc[k].replace(" ","_"))
+            file_name=di1+A[1].iloc[k].replace(" ","_")
+            url_photo=A[3].iloc[k]
+            if url_photo.find("nopicture")<0:
+                n=int(url_photo.find("nopicture")<0)
+                web4photo(url_photo,file_name+'.jpg')
+            response =http.request('GET',url_wiki+A[1].iloc[k].replace(" ","_"))
             soup = BeautifulSoup(response.data)
             dt=pd.Series(soup.find_all("tbody"))
             s2=BeautifulSoup(str(dt[0]))
@@ -93,21 +95,15 @@ for gen in GENERO:
                     aa1=pd.Series(list(set(aa1.values.reshape(aa1.size).tolist())-{None}))
                     ss=aa1.str.find(".jpg/")
                     aa1='https:'+pd.unique(aa1[ss>0].str.split('.jpg/',expand=True)[0])+'.jpg'
-                    if len(aa1)>photosTotal:
-                        ss=[]
-                        l=0
-                        while (len(ss)<photosTotal) and (l<photosTotal):
-                            ss1=pd.unique(np.random.randint(len(aa1),size=photosTotal)).tolist()
-                            ss=list(set(ss+ss1))
-                            l+=1
-                        if len(ss)>photosTotal:
-                            ss=range(photosTotal-1)
+                    if len(aa1)>photos_totales:
+                        ss=np.random.permutation(len(aa1)).tolist()[:photos_totales]
                         aa1=aa1[ss]                        
                     for g,h in enumerate(aa1):                
                         im2+=[g+n]
-                        web4photo(h,filename+'_'+str(g+n)+'.jpg')
+                        web4photo(h,file_name+'_'+str(g+n)+'.jpg')
             #Photography of last year
-            response =http.request('GET',"https://www.gettyimages.es/fotos/{}?compositions=headshot&family=editorial&numberofpeople=one&recency=last12months&sort=best".format(A[1].iloc[k].lower().replace(" ","_")))
+            url_gettyimages="https://www.gettyimages.com/photos/{n[0]}?compositions=headshot&family=editorial&numberofpeople=one&phrase={n[1]}&recency=last12months&sort=newest".format(n=changeFormat(A[1].iloc[k]))
+            response =http.request('GET',url_gettyimages)
             soup = BeautifulSoup(response.data)
             s3=soup.find_all("img")
             ultima=False
@@ -119,26 +115,26 @@ for gen in GENERO:
                     ultima=True
                     S=S[P>0].reset_index(drop=True)
                     S=S.str.split('"',expand=True)[5]
-                    if photosTotal<S.shape[0]:
-                        aa=pd.unique(np.random.randint(S.shape[0],size=photosTotal)).tolist()
+                    if photos_totales<S.shape[0]:
+                        aa=np.random.permutation(S.shape[0]).tolist()[:photos_totales]
                     else:
                         aa=range(S.shape[0])
                     for g,h in enumerate(S.iloc[aa]):
-                        web4photo(h,filename+'_lastyear_'+str(g)+'.jpg')
+                        web4photo(h,file_name+'_lastyear_'+str(g)+'.jpg')
             if n>0:
                 name=A[1].iloc[k]
                 bday=str(s2.find_all("span")[1]).split('"bday">')[1].split("</span>")[0]
-                old=humanfriendly.format_timespan(datetime.datetime.today()-datetime.datetime.strptime(bday, '%Y-%m-%d'),max_units=1)
-                old=int(old.split(" ")[0])
-                dic={'Nombre':[name],"genero":[gen],"Nacimiento":[bday],"Edad":[old]}
-                if url.find("nopicture")<0:
-                    dic.update({"img1":[filename+'.jpg']})
+                edad=humanfriendly.format_timespan(datetime.datetime.today()-datetime.datetime.strptime(bday, '%Y-%m-%d'),max_units=1)
+                edad=int(edad.split(" ")[0])
+                dic={'Nombre':[name],"genero":[gen],"Nacimiento":[bday],"Edad":[edad]}
+                if url_photo.find("nopicture")<0:
+                    dic.update({"img1":[file_name+'.jpg']})
                 if len(im2)>0:
                     for g in im2:
-                        dic.update({"img"+str(k):[filename+'_'+str(g)+'.jpg']})
+                        dic.update({"img"+str(k):[file_name+'_'+str(g)+'.jpg']})
                 if ultima:
                     for g in range(len(aa)):
-                        dic.update({"img_lastyear"+str(g):[filename+'_lastyear_'+str(g)+'.jpg']})
+                        dic.update({"img_lastyear"+str(g):[file_name+'_lastyear_'+str(g)+'.jpg']})
                 S=pd.DataFrame.from_dict(dic)
                 if start:
                     DFR=S
