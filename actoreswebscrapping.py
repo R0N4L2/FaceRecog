f81b53bb02fba5709e75f02988c09359a0995af2
Ronald Castillo Capino <6721335+R0N4L2@users.noreply.github.com>
2019-05-18 21:02:46 -0500

    Add files via upload

--- actoreswebscrapping.py
+++ actoreswebscrapping.py
@@ -10,7 +10,7 @@ import numpy as np
 import shutil
 from os.path import dirname, abspath, join, exists
 import pickle
-
+import unidecode
 def writePickle(df,file_name):
 	with open(file_name, 'wb') as f:
 		pickle.dump(df, f, pickle.HIGHEST_PROTOCOL)
@@ -23,7 +23,7 @@ def writeCSV(df,file_name,useIndex = False):
 	df.to_csv(file_name,index=useIndex)	
 def writeExcel(df,file_name,useIndex = False):
 	df.to_excel(file_name,index=useIndex)
-def writeFile(df,filName,useIndex = False):
+def writeFile(df,file_name,useIndex = False):
 	if file_name.endswith('.csv'):
 		writeCSV(df,file_name,useIndex = useIndex)
 	if file_name.endswith('.xlsx'):
@@ -60,8 +60,8 @@ for gen in GENERO:
     if not exists(join(di,gen+'\\')):
         os.mkdir(join(di,gen+'\\'))
     di1=join(di,gen+'\\')
-    for k in range(0,min(actores_totales,10000),50):
-        url_imdb = "https://www.imdb.com/search/name?gender="+gen+"&start="+str(1+k)+"&ref_=rlm"
+    for j in range(0,min(actores_totales,10000),50):
+        url_imdb = "https://www.imdb.com/search/name?gender="+gen+"&start="+str(1+j)+"&ref_=rlm"
         response = http.request('GET',url_imdb)
         soup = BeautifulSoup(response.data)
         s=pd.Series(soup.find_all("img"))
@@ -71,74 +71,77 @@ for gen in GENERO:
         A[3]=A[3].str.split('"',expand=True)[1]
         A=A.reset_index(drop=True)
         for k in range(A.shape[0]):
-            file_name=di1+A[1].iloc[k].replace(" ","_")
+            name=unidecode.unidecode(A[1].iloc[k])
+            file_name=di1+name.replace(" ","_")
             url_photo=A[3].iloc[k]
             if url_photo.find("nopicture")<0:
                 n=int(url_photo.find("nopicture")<0)
                 web4photo(url_photo,file_name+'.jpg')
-            response =http.request('GET',url_wiki+A[1].iloc[k].replace(" ","_"))
+            response =http.request('GET',url_wiki+name.replace(" ","_"))
             soup = BeautifulSoup(response.data)
             dt=pd.Series(soup.find_all("tbody"))
-            s2=BeautifulSoup(str(dt[0]))
-            im2=[]
-            if len(s2.find_all("img"))>0:
-                n+=1
-                aa1=pd.Series(str(s2.find_all("img")[0]).split('"'))
-                ss=aa1.str.find(".jpg")
-                aa1=aa1[ss>0].reset_index(drop=True)
-                ss=aa1.str.find("wikipedia")
-                aa1=aa1[ss>0].reset_index(drop=True)
-                if any(aa1.str.find(",")>0):
-                    aa1=aa1.str.split(',',expand=True)
-                    aa1=aa1.values.reshape(aa1.size)
-                    aa1=pd.Series(list(set(aa1.tolist())-{None})).str.split(' ',expand=True)
-                    aa1=pd.Series(list(set(aa1.values.reshape(aa1.size).tolist())-{None}))
-                    ss=aa1.str.find(".jpg/")
-                    aa1='https:'+pd.unique(aa1[ss>0].str.split('.jpg/',expand=True)[0])+'.jpg'
-                    if len(aa1)>photos_totales:
-                        ss=np.random.permutation(len(aa1)).tolist()[:photos_totales]
-                        aa1=aa1[ss]                        
-                    for g,h in enumerate(aa1):                
-                        im2+=[g+n]
-                        web4photo(h,file_name+'_'+str(g+n)+'.jpg')
-            #Photography of last year
-            url_gettyimages="https://www.gettyimages.com/photos/{n[0]}?compositions=headshot&family=editorial&numberofpeople=one&phrase={n[1]}&recency=last12months&sort=newest".format(n=changeFormat(A[1].iloc[k]))
-            response =http.request('GET',url_gettyimages)
-            soup = BeautifulSoup(response.data)
-            s3=soup.find_all("img")
-            ultima=False
-            if len(s3)>0:
-                S=pd.Series(s3).astype(str)
-                P=S.str.find(A[1].iloc[k])
-                if any(P>0):
+            if any(dt.astype(str).str.find("bday")>0):
+                s2=BeautifulSoup(dt[dt.astype(str).str.find("bday")>0].astype(str).iloc[0])
+                im2=[]
+                if len(s2.find_all("img"))>0:
                     n+=1
-                    ultima=True
-                    S=S[P>0].reset_index(drop=True)
-                    S=S.str.split('"',expand=True)[5]
-                    if photos_totales<S.shape[0]:
-                        aa=np.random.permutation(S.shape[0]).tolist()[:photos_totales]
+                    aa1=pd.Series(str(s2.find_all("img")[0]).split('"'))
+                    ss=aa1.str.find(".jpg")
+                    aa1=aa1[ss>0].reset_index(drop=True)
+                    ss=aa1.str.find("wikipedia")
+                    aa1=aa1[ss>0].reset_index(drop=True)
+                    if any(aa1.str.find(",")>0):
+                        aa1=aa1.str.split(',',expand=True)
+                        aa1=aa1.values.reshape(aa1.size)
+                        aa1=pd.Series(list(set(aa1.tolist())-{None})).str.split(' ',expand=True)
+                        aa1=pd.Series(list(set(aa1.values.reshape(aa1.size).tolist())-{None}))
+                        ss=aa1.str.find(".jpg/")
+                        aa1='https:'+pd.unique(aa1[ss>0].str.split('.jpg/',expand=True)[0])+'.jpg'
+                        if len(aa1)>photos_totales:
+                            ss=np.random.permutation(len(aa1)).tolist()[:photos_totales]
+                            aa1=aa1[ss]                        
+                        for g,h in enumerate(aa1):                
+                            im2+=[g+n]
+                            web4photo(h,file_name+'_'+str(g+n)+'.jpg')
+                #Photography of last year
+                url_gettyimages="https://www.gettyimages.com/photos/{n[0]}?compositions=headshot&family=editorial&numberofpeople=one&phrase={n[1]}&recency=last12months&sort=newest".format(n=changeFormat(name))
+                response =http.request('GET',url_gettyimages)
+                soup = BeautifulSoup(response.data)
+                s3=soup.find_all("img")
+                ultima=False
+                if len(s3)>0:
+                    S=pd.Series(s3).astype(str)
+                    P=S.str.find(name)
+                    if any(P>0):
+                        n+=1
+                        ultima=True
+                        S=S[P>0].reset_index(drop=True)
+                        S=S.str.split('"',expand=True)[5]
+                        if photos_totales<S.shape[0]:
+                            aa=np.random.permutation(S.shape[0]).tolist()[:photos_totales]
+                        else:
+                            aa=range(S.shape[0])
+                        for g,h in enumerate(S.iloc[aa]):
+                            web4photo(h,file_name+'_lastyear_'+str(g)+'.jpg')
+                if n>0:
+                    nombre=A[1].iloc[k]
+                    s2=pd.Series(s2.find_all("span")).astype(str)
+                    bday=s2[s2.str.find("bday")>0].iloc[0].split('"bday">')[1].split("</span>")[0]
+                    edad=humanfriendly.format_timespan(datetime.datetime.today()-datetime.datetime.strptime(bday, '%Y-%m-%d'),max_units=1)
+                    edad=int(edad.split(" ")[0])
+                    dic={'Nombre':[nombre],"genero":[gen],"Nacimiento":[bday],"Edad":[edad]}
+                    if url_photo.find("nopicture")<0:
+                        dic.update({"img1":[file_name+'.jpg']})
+                    if len(im2)>0:
+                        for g in im2:
+                            dic.update({"img"+str(k):[file_name+'_'+str(g)+'.jpg']})
+                    if ultima:
+                        for g in range(len(aa)):
+                            dic.update({"img_lastyear"+str(g):[file_name+'_lastyear_'+str(g)+'.jpg']})
+                    S=pd.DataFrame.from_dict(dic)
+                    if start:
+                        DFR=S
+                        start=False
                     else:
-                        aa=range(S.shape[0])
-                    for g,h in enumerate(S.iloc[aa]):
-                        web4photo(h,file_name+'_lastyear_'+str(g)+'.jpg')
-            if n>0:
-                name=A[1].iloc[k]
-                bday=str(s2.find_all("span")[1]).split('"bday">')[1].split("</span>")[0]
-                edad=humanfriendly.format_timespan(datetime.datetime.today()-datetime.datetime.strptime(bday, '%Y-%m-%d'),max_units=1)
-                edad=int(edad.split(" ")[0])
-                dic={'Nombre':[name],"genero":[gen],"Nacimiento":[bday],"Edad":[edad]}
-                if url_photo.find("nopicture")<0:
-                    dic.update({"img1":[file_name+'.jpg']})
-                if len(im2)>0:
-                    for g in im2:
-                        dic.update({"img"+str(k):[file_name+'_'+str(g)+'.jpg']})
-                if ultima:
-                    for g in range(len(aa)):
-                        dic.update({"img_lastyear"+str(g):[file_name+'_lastyear_'+str(g)+'.jpg']})
-                S=pd.DataFrame.from_dict(dic)
-                if start:
-                    DFR=S
-                    start=False
-                else:
-                    DFR=DFR.append(S)                
+                        DFR=DFR.append(S)                
 writeFile(DFR,di+'FaceRecognition.pickle')
